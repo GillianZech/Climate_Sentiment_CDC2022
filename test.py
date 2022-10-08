f42778f64ae81a40c07642b933397b9a5dbe3263
Simi Singh <34402769+archeopsor@users.noreply.github.com>
2022-10-08 16:28:40 -0400

    fixed csv

--- test.py
+++ test.py
@@ -15,15 +15,21 @@ def grouper(iterable, n, fillvalue=None):
 # data = pd.DataFrame(columns=["id", "text", 'favorite_count', 'retweet_count', 'created_at', 'coordinates'])
 data = pd.read_csv("Dataset.csv")
 
-# Read IDs from .txt file and get tweets
-with open("climate_id.txt.03", 'r') as f:
-    for lines in grouper(f, 25000, ''):
-        ids = [x[:-1] for x in lines]
-        ids = ids[::250]
-        tweets = api.lookup_statuses(ids)
-        for tweet in tweets:
-            data = data.append({"id": tweet.id, 'text': tweet.text, 'favorite_count': tweet.favorite_count, 'retweet_count': tweet.retweet_count, 'created_at': tweet.created_at, 'coordinates': tweet.coordinates}, ignore_index=True)
-        print(len(data))
+for filename in ['climate_id.txt.00', 'climate_id.txt.01', 'climate_id.txt.02', 'climate_id.txt.03']:
+    # Read IDs from .txt file and get tweets
+    with open(filename, 'r') as f:
+        f.readline() # To ensure no duplicate IDs are used
+
+        # Only read every 200th tweet
+        for lines in grouper(f, 20000, ''):
+            ids = [x[:-1] for x in lines]
+            ids = ids[::200]
+            tweets = api.lookup_statuses(ids)
+            for tweet in tweets:
+                data = pd.concat([data, pd.Series({"id": tweet.id, 'text': tweet.text, 'favorite_count': tweet.favorite_count, 'retweet_count': tweet.retweet_count, 'created_at': tweet.created_at, 'coordinates': tweet.coordinates})], ignore_index=True)
+            print(len(data))
+        
+    print(f'{filename} done')
 
 
 data.to_csv("Dataset.csv")
