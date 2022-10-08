5fba683d4f8ba6053b535ae0e5df4eb79d179025
Gillian Zech <ginger01@email.unc.edu>
2022-10-08 18:54:46 -0400

    still working on frequencies

--- frequency.py
+++ frequency.py
@@ -1,24 +1,25 @@
 import nltk
-from nltk.classify import NaiveBayesClassifier
-from nltk.corpus import subjectivity
-from nltk.sentiment import SentimentAnalyzer
-from nltk.sentiment.util import *
-nltk.download('subjectivity')
-nltk.download('punkt')
-nltk.download('vader_lexicon')
-from nltk import tokenize
-
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+# nltk.download()
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
 import pandas as pd
 import csv
 
-def analyze(sentence):
-    sid = SentimentIntensityAnalyzer()
-    ss = sid.polarity_scores(sentence)
-    return ss
-    for k in sorted(ss):
-        print('{0}: {1}, '.format(k, ss[k]), end='')
 
-data = pd.read_csv(".csv")
-data['score'] = data['text'].apply(analyze)
-data.to_csv("frequencies")
+def find_frequency(list):
+    # tokens = nltk.word_tokenize(list)
+    f_dist = FreqDist(list)
+    print(f_dist)
+    f_dist.plot()
+    return f_dist
+full_list = []
+def make_list(tweet):
+    tokens = nltk.word_tokenize(tweet)
+    for token in tokens:
+        full_list.append(token)
+
+data = pd.read_csv("nltk_split.csv").head(5)
+data2 = pd.DataFrame(columns=["word", "frequency"])
+make_list = data['text'].apply(make_list)
+data2['frequency'] = make_list(find_frequency)
+# data.to_csv("frequencies")
