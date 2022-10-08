2805a3f29178ab1db47660de95ee856721f60e7d
Simi Singh <34402769+archeopsor@users.noreply.github.com>
2022-10-08 12:06:25 -0400

    add ids

--- test.py
+++ test.py
@@ -4,3 +4,11 @@ from secrets import API_KEY, SECRET_KEY, ACCESS_TOKEN, SECRET_TOKEN
 auth = tw.OAuthHandler(API_KEY, SECRET_KEY)
 auth.set_access_token(ACCESS_TOKEN, SECRET_TOKEN)
 api = tw.API(auth, wait_on_rate_limit=True)
+
+# Read IDs from .txt file and get tweets
+ids = []
+with open("climate_id.txt.00", 'r') as f:
+    for _ in range(100):
+        ids.append(f.readline()[:-1])
+
+    tweets = api.lookup_statuses(ids)
