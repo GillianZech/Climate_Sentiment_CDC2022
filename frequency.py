6d21783469386331b3d47470b333a715deceb270
Simi Singh <34402769+archeopsor@users.noreply.github.com>
2022-10-08 23:22:18 -0400

    finalized frequency csv

--- frequency.py
+++ frequency.py
@@ -3,6 +3,7 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
 from nltk.probability import ConditionalFreqDist
+from nltk.corpus import stopwords, words
 import pandas as pd
 import csv
 import sqlite3
@@ -37,17 +38,17 @@ full_list = []
 def make_list_function(tweet):
     tokens = nltk.word_tokenize(tweet)
     for token in tokens:
-        full_list.append(token)
+        full_list.append(token.lower())
 
 data = pd.read_csv("nltk_split.csv")
 data2 = pd.DataFrame(columns=["word", "frequency"])
 data['text'].apply(make_list_function)
-print("Making frequency string")
 frequency_string = find_frequency(full_list).pformat(len(full_list))
 frequency_dict = ast.literal_eval(frequency_string[9:-1])
 frequency_series = pd.Series(frequency_dict)
 frequency_series = frequency_series[frequency_series.index.str.isalnum()]
-frequency_series = frequency_series.drop(labels=['RT'])
+frequency_series = frequency_series[~frequency_series.index.str.lower().isin(stopwords.words('english'))]
+frequency_series = frequency_series[frequency_series.index.str.lower().isin(words.words())]
 print(frequency_series)
 frequency_series.to_csv("frequency.csv")
 
