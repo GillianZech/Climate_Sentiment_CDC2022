432e5b19e69694e89dbd544b9b1feeb685663735
Simi Singh <34402769+archeopsor@users.noreply.github.com>
2022-10-08 22:53:07 -0400

    Frequency code finished

--- frequency.py
+++ frequency.py
@@ -2,6 +2,7 @@ import nltk
 # nltk.download()
 from nltk.tokenize import word_tokenize
 from nltk.probability import FreqDist
+from nltk.probability import ConditionalFreqDist
 import pandas as pd
 import csv
 import sqlite3
@@ -25,24 +26,32 @@ import ast
 def find_frequency(list):
     # tokens = nltk.word_tokenize(list)
     f_dist = FreqDist(list)
+    # for word in list:
+    #     condition = word.isalnum()
+    #     f_dist[condition][word] += 1
     print(f_dist)
-    f_dist.plot(20)
-    return f_dist
+    # to_return = f_dist.r_Nr()
+    f_dist.plot()
+    return f_dist #to_return
 full_list = []
 def make_list_function(tweet):
     tokens = nltk.word_tokenize(tweet)
     for token in tokens:
         full_list.append(token)
 
-data = pd.read_csv("nltk_split.csv").head(50)
+data = pd.read_csv("nltk_split.csv")
 data2 = pd.DataFrame(columns=["word", "frequency"])
 data['text'].apply(make_list_function)
+# print(find_frequency(full_list))
 frequency_string = find_frequency(full_list).pformat(len(full_list))
 # data2['frequency'] = find_frequency(full_list)
 # data.to_csv("frequencies")
 frequency_dict = ast.literal_eval(frequency_string[9:-1])
 frequency_series = pd.Series(frequency_dict)
+frequency_series = frequency_series[frequency_series.index.str.isalnum()]
+frequency_series = frequency_series.drop(labels=['RT'])
 print(frequency_series)
+frequency_series.to_csv("frequency.csv")
 
 # def count_words(list):
 #     freq = {}
